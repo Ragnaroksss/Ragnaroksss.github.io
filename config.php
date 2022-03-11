c469282aab935486bef97dffafd3583857e51085
Ragnaroksss <52845220+Ragnaroksss@users.noreply.github.com>
2022-03-11 09:10:13 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+$key0='154322';//管理员密码
+
+$pagesize='15';//文件列表每页显示多少个文件，必须为整数
+
+$Storage = 'udisk';//Storage名称
+
+$openapi = 1;//是否开启API上传
+
+/*数据库信息*/
+$host = 'localhost'; //数据库服务器
+$port = 3306; //数据库端口
+$user = 'wdxaisaj'; //数据库用户名
+$pwd = '154322'; //数据库密码
+$dbname = 'wdxaisaj'; //数据库名称
+
+?>
